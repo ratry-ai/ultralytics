c12552ea090dd4b755645e25747891f6e34ef490
ratry-ai <ratrymutmainnah764@gmail.com>
2026-08-11 20:10:50 +0800

    Update loss.py
    Signed-off-by: ratry-ai <ratrymutmainnah764@gmail.com>

--- ultralytics/utils/loss.py
+++ ultralytics/utils/loss.py
@@ -423,7 +423,7 @@ class v8DetectionLoss:
     # Decode bbox
         pred_bboxes = self.bbox_decode(anchor_points, pred_distri)
 
-        target_bboxes, target_scores, fg_mask, target_gt_idx = self.assigner(
+        target_labels, target_bboxes, target_scores, fg_mask, target_gt_idx = self.assigner(
         pred_scores.detach().sigmoid(),
         (pred_bboxes.detach() * stride_tensor).type(gt_bboxes.dtype),
         anchor_points * stride_tensor,
